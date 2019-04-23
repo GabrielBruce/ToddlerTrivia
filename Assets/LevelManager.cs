c7b82eb9c4ee952f96695e2d0563cb3da17af166
Gabriel <johngabrielbruce@gmail.com>
2019-04-22 21:10:47 -0500

    Initial Commit Basic Menu System Background image Persistent background music Scene navigation Menu scene 	Start button 	Quit button Prototype Game scene 	Back button

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float menuVolume = 1f;
+    public float gameVolume = 0.5f;
+
+    public void LoadLevel(string name) {
+        Debug.Log("New Level load: " + name);
+        if (name == "Menu")
+        {
+            FindObjectOfType<AudioSource>().volume = menuVolume;
+        }
+        else
+        {
+            FindObjectOfType<AudioSource>().volume = gameVolume;
+        }
+        SceneManager.LoadScene(name);
+    }
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+}
---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
+
+	void Start () {
+
+		if (instance != null && instance != this) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing!");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+}
